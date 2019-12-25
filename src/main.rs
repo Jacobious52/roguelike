8a4017da68269cb6a88bf1cbb9c11b5e268ce1cb
Jacobious52 <jacobgonzalez5252@gmail.com>
2019-12-26 00:11:49 +1100

    part 3 cleaned

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+rltk::add_wasm_support!();
+
+use rltk::{Console, GameState, Rltk, RGB};
+use specs::prelude::*;
+#[macro_use]
+extern crate specs_derive;
+
+mod components;
+pub use components::*;
+
+mod player;
+pub use player::*;
+
+mod map;
+pub use map::*;
+
+mod rect;
+
+pub struct State {
+    ecs: World,
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        self.ecs.maintain();
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+
+        player_input(self, ctx);
+
+        self.run_systems();
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        draw_map(&self.ecs, ctx);
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn main() {
+    let context = Rltk::init_simple8x8(80, 50, "Hello Rust World", "resources");
+    let mut gs = State { ecs: World::new() };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
+    gs.ecs.register::<Viewshed>();
+
+    let map: Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
+    gs.ecs.insert(map);
+
+    gs.ecs
+        .create_entity()
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Player {})
+        .build();
+
+    rltk::main_loop(context, gs);
+}
